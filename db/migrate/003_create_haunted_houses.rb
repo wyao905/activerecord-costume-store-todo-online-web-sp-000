313af755ec70058005e22704d97a7a5ef2acc29f
Warren Yao <warrenyca905@gmail.com>
2019-08-15 01:20:32 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,13 +3,13 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   def change
     create_table :haunted_houses do |t|
-      t.string :name,
-      t.string :location,
-      t.string :theme,
-      t.float :price,
-      t.boolean :whether_theyre_family_friendly_or_not,
-      t.string :opening_date,
-      t.string :closing_date,
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :whether_theyre_family_friendly_or_not
+      t.string :opening_date
+      t.string :closing_date
       t.text :long_description
     end
   end
