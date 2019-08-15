ccaa67e31fa1a471f9c03e74491d4ad24e31c41c
Warren Yao <warrenyca905@gmail.com>
2019-08-15 01:22:33 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -6,10 +6,10 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :location
       t.integer :costume_inventory
-      t.integer :number_of_employees
-      t.boolean :whether_or_not_its_still_in_business
-      t.string :opening_time
-      t.string :closing_time
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
 end
