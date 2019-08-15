ee18edee2bb39534c738ae3dedb925c7355dcf28
Warren Yao <warrenyca905@gmail.com>
2019-08-15 01:20:25 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,12 +3,12 @@
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
   def change
     create_table :costume_stores do |t|
-      t.string :name,
-      t.string :location,
-      t.integer :costume_inventory,
-      t.integer :number_of_employees,
-      t.boolean :whether_or_not_its_still_in_business,
-      t.string :opening_time,
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :number_of_employees
+      t.boolean :whether_or_not_its_still_in_business
+      t.string :opening_time
       t.string :closing_time
     end
   end
