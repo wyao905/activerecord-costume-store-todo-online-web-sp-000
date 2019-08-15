d77f8b165bed4f517e4f93742f276c1f8e7e92d7
Warren Yao <warrenyca905@gmail.com>
2019-08-15 01:09:00 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,11 @@
 # Create your haunted_houses migration here
+
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+  create_table :haunted_houses do |t|
+    t.string :name,
+    t.float :price,
+    t.string :size,
+    t.string :image_url,
+    t.timestamp
+  end
+end
