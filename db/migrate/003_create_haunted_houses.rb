a42cb0fb374e9de996c882ea611a84bd7c8490c4
Warren Yao <warrenyca905@gmail.com>
2019-08-15 01:10:17 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -3,9 +3,12 @@
 class CreateHauntedHouses < ActiveRecord::Migration[5.1]
   create_table :haunted_houses do |t|
     t.string :name,
+    t.string :location,
+    t.string :theme,
     t.float :price,
-    t.string :size,
-    t.string :image_url,
-    t.timestamp
+    t.boolean :whether_theyre_family_friendly_or_not,
+    t.string :opening_date,
+    t.string :closing_date,
+    t.text :long_description
   end
 end
